9f0cd8ef9dc1c8926e174200523f35c95610649a
joseph mwanzia <makamwanzia@gmail.com>
2023-02-17 00:50:46 +0300

    Initial Commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,69 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8 )
+        @name = name
+        @bank_account= bank_account
+        @happiness = happiness 
+        @hygiene = hygiene
+    end
+
+    def happiness=(value)
+        @happiness= value
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+    end
+
+    def hygiene=(value)
+        @hygiene = value
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end 
+
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        self.call_frien
+    end
+end
+
+
+# blake = Person.new("Blake")
+# puts blake.name
+# puts blake.bank_account
+
+
+# binding.pry
